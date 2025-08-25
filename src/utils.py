0cb7a414311071c4bdb1811eaccfee2d0094ee39
trisha15-n <nagartrisha15@gmail.com>
2025-08-25 19:58:55 +0530

    ModelTrainer

--- src/utils.py
+++ src/utils.py
@@ -16,3 +16,24 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        from sklearn.metrics import r2_score
+        model_report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            model_report[list(models.keys())[i]] = test_model_score
+
+        return model_report
+    except Exception as e:
+        raise CustomException(e, sys)    
